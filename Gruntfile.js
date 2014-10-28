8a612d1c866d7a77e08ee5324f985619798d1d84
patgod85 <patgod85@gmail.com>
2014-10-28 09:18:05 +0300

    Deploy task for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,11 +19,32 @@ module.exports = function(grunt) {
                 options: {
                     specs: 'spec/*Spec.js',
                     vendor: [
-                        "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"
-                        //"bower_components/jquery/dist/jquery.min.js"
+                        "bower_components/jquery/dist/jquery.min.js"
                     ]
                 }
 
+            }
+        },
+        'ftp-deploy': {
+            build: {
+                auth: {
+                    host: '77.222.40.121',
+                    port: 21
+                },
+                src: 'upload',
+                dest: '/patgod/public_html/graph/',
+                exclusions: []
+            }
+        },
+        clean: [
+            "upload"
+        ],
+        copy: {
+            main: {
+                files: [
+                    {expand: true, src: ['example/*'], dest: 'upload/', filter: 'isFile'},
+                    {expand: true, src: ['build/*'], dest: 'upload/', filter: 'isFile'}
+                ]
             }
         }
     });
@@ -31,9 +52,13 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-ftp-deploy');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Default task(s).
     grunt.registerTask('default', ['jasmine', 'uglify']);
+
+    grunt.registerTask('deploy', ['clean', 'copy', 'ftp-deploy'])
 };
